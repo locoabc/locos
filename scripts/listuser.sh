df1d8236d21db68bc11dc0964c60fff0ca1b59e7
T.J. Yang <tjyang@lenovo.com>
2023-12-29 14:49:00 -0600

    adding list user

---
+++ scripts/listuser.sh
@@ -0,0 +1,6 @@
+
+curl --location --request GET '127.0.0.1:3000/api/user/current' \
+     --header 'Content-Type: application/json' \
+     --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJwaWQiOiJjNjIxMGJmMi02YmE1LTRiOTYtOTFjMS1lZmQyMTc5ZjI1NDYiLCJleHAiOjE3MDQwNzE3NzJ9.qv16Ni5sh8o6x9C9lSAsh15GGDHDHmzeIK_CBZGop6lW8g0Pc11rbeJExqsln-aefmYhw3AyHfwmhhZoa4WjrA'
+echo
+
