f714c551f973aeb7e2b1842691108a966bf9c816
T.J. Yang <tjyang@lenovo.com>
2024-01-13 09:33:58 -0600

    Automated commit message without details

--- claps/src/bin/sub03.rs
+++ claps/src/bin/sub03.rs
@@ -7,17 +7,45 @@ fn main() {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
-            Command::new("add")
-                .about("Adds files to myapp")
+            Command::new("create")
+                .about("C-RUD ...")
+                .arg(arg!([NAME])),
+        )
+        .subcommand(
+            Command::new("retrieve")
+                .about("C-R-UD ...")
+                .arg(arg!([NAME])),
+        )
+        .subcommand(
+            Command::new("update")
+                .about("CR-U-D ...")
+                .arg(arg!([NAME])),
+        )
+        .subcommand(
+            Command::new("delete")
+                .about("CRU-D ...")
                 .arg(arg!([NAME])),
         )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("add", sub_matches)) => println!(
-            "'myapp add' was used, name is: {:?}",
+        Some(("create", sub_matches)) => println!(
+            "'myapp create' was used, name is: {:?}",
             sub_matches.get_one::<String>("NAME")
         ),
+        Some(("retrieve", sub_matches)) => println!(
+            "'myapp retrieve' was used, name is: {:?}",
+            sub_matches.get_one::<String>("NAME")
+        ),
+        Some(("update", sub_matches)) => println!(
+            "'myapp update' was used, name is: {:?}",
+            sub_matches.get_one::<String>("NAME")
+        ),
+        Some(("delete", sub_matches)) => println!(
+            "'myapp delete' was used, name is: {:?}",
+            sub_matches.get_one::<String>("NAME")
+        ),
+
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
 }
