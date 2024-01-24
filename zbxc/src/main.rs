a94e82b4a03b271a32eb543df7b116ab989af1a6
T.J. Yang <tjyang@lenovo.com>
2024-01-24 14:14:09 -0600

    Automated commit message without details

--- zbxc/src/main.rs
+++ zbxc/src/main.rs
@@ -1,18 +1,21 @@
+use log::error;
 use reqwest::blocking::Client;
 use zabbix_api::client::v6::ZabbixApiV6Client;
 use zabbix_api::client::ZabbixApiClient;
-use log::error;
 
 fn main() {
-  let http_client = Client::new();
+    let http_client = Client::new();
 
-  let client = ZabbixApiV6Client::new(http_client, "https://ilclnagios03.mot.com/zabbix/api_jsonrpc.php");
-    
-  match client.get_auth_session("Admin", "zabbix") {
-    Ok(session) => println!("session: {session}"),
-    Err(e) => {
-        error!("error: {}", e);
-        panic!("unexpected error")
+    let client = ZabbixApiV6Client::new(
+        http_client,
+        "https://zabbix03.test.com/zabbix/api_jsonrpc.php",
+    );
+
+    match client.get_auth_session("Admin", "zabbix") {
+        Ok(session) => println!("session: {session}"),
+        Err(e) => {
+            error!("error: {}", e);
+            panic!("unexpected error")
+        }
     }
-  }
 }
